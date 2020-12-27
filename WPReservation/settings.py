31aa60edfd9d181bfc3ec4cdd4a88cbaf1ff0246
mnogoruk <lionless072@gmail.com>
2020-12-27 17:12:44 +0300

    mySQL connection

--- WPReservation/settings.py
+++ WPReservation/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'WPReservation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wpreservation',
+        'USER': 'dj',
+        'PASSWORD': 'ZEHmE5Cj',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
