3231dd5967caae021864de7362229d488b7ec48c
mnogoruk <lionless072@gmail.com>
2020-12-27 18:50:03 +0300

    fields parameter added to serializer

--- api/serializer.py
+++ api/serializer.py
@@ -5,13 +5,16 @@ from .models import Cabinet, Workspace, WorkspaceReservation
 class CabinetSerializer(ModelSerializer):
     class Meta:
         model = Cabinet
+        fields = '__all__'
 
 
 class WorkspaceSerializer(ModelSerializer):
     class Meta:
         model = Workspace
+        fields = '__all__'
 
 
 class WorkspaceReservationSerializer(ModelSerializer):
     class Meta:
         model = WorkspaceReservation
+        fields = '__all__'
